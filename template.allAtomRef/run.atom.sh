f1f2c1650bd8c5d93018b8a894751262f5eed719
wanghan <wanghan@a28d8dd4-3dfb-4ec0-b98a-e5cf011a4b41>
2011-10-21 14:59:19 +0000

    revise run.atom.sh

--- template.allAtomRef/run.atom.sh
+++ template.allAtomRef/run.atom.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-nbox=8
+nbox=4
 scale=0.92
 
 runtime_log=run.log
@@ -9,24 +9,26 @@ make_log=make.log
 rm -f $runtime_log $make_log
 
 make -C tools/gen.conf/ -j4 >> $make_log
-cp tools/gen.conf/spc216.gro .
+rm -f spc216.gro; cp tools/gen.conf/spc216.gro .
 
-echo "# genconf by spc216     =================================================================="
+echo "# genconf by spc216     ==================================================================" >> $runtime_log
 genconf -nbox $nbox $nbox $nbox -f spc216.gro -o conf.gro &>> $runtime_log
-echo "# end genconf by spc216 =================================================================="
-echo "# add wall              =================================================================="
+echo "# end genconf by spc216 ==================================================================" >> $runtime_log
+echo "# add wall              ==================================================================" >> $runtime_log
 tools/gen.conf/bcc.wall -f conf.gro -o out.gro &>> $runtime_log
-echo "# end add wall          =================================================================="
-echo "# rescale box           =================================================================="
+echo "# end add wall          ==================================================================" >> $runtime_log
+echo "# rescale box           ==================================================================" >> $runtime_log
 editconf -scale $scale $scale $scale -f out.gro -o conf.gro &>> $runtime_log
-echo "# end rescale box       =================================================================="
+echo "# end rescale box       ==================================================================" >> $runtime_log
 
+grocleanit
+rm -f *.itp grompp.mdp
 cp tools/script.atom/*itp .
-cp tools/script.atom/grompp.gro .
+cp tools/script.atom/grompp.mdp .
 
-echo "# grompp                =================================================================="
+echo "# grompp                ==================================================================" >> $runtime_log
 grompp &>> $runtime_log
-echo "# end grompp            =================================================================="
-echo "# mdrun                 =================================================================="
-mdrun &>> $runtime_log
-echo "# end mdrun             =================================================================="
+echo "# end grompp            ==================================================================" >> $runtime_log
+echo "# mdrun                 ==================================================================" >> $runtime_log
+mdrun -v &>> $runtime_log
+echo "# end mdrun             ==================================================================" >> $runtime_log
