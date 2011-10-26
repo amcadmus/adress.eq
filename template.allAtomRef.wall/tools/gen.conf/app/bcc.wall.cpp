2723a79970812152308cca36e303f7282fae5f65
wanghan <wanghan@a28d8dd4-3dfb-4ec0-b98a-e5cf011a4b41>
2011-10-26 17:12:41 +0000

    change name: template.allAtomRef->template.allAtomRef.wall

--- template.allAtomRef.wall/tools/gen.conf/app/bcc.wall.cpp
+++ template.allAtomRef.wall/tools/gen.conf/app/bcc.wall.cpp
@@ -119,14 +119,8 @@ int main (int argc, char * argv[])
   for (int ix = 0; ix < nx; ++ix){
     for (int iy = 0; iy < ny; ++iy){
       for (int iz = 0; iz < nz; ++iz){
-	if (natom != 0){
-	  resdindex[natom+indexShift] = resdindex[natom-1] + indexShift + 1;
-	  atomindex[natom+indexShift] = atomindex[natom-1] + indexShift + 1;
-	}
-	else {
-	  resdindex[natom+indexShift] =  + indexShift + 1;
-	  atomindex[natom+indexShift] =  + indexShift + 1;
-	}
+	resdindex[natom+indexShift] = resdindex[natom-1] + indexShift + 1;
+	atomindex[natom+indexShift] = atomindex[natom-1] + indexShift + 1;
 	resdname[natom+indexShift] = "WA";
 	atomname[natom+indexShift] = "C";
 	posi[natom+indexShift].resize(3);
@@ -135,14 +129,8 @@ int main (int argc, char * argv[])
 	posi[natom+indexShift][2] = (iz+0.25) * hh[2];
 	velo[natom+indexShift].resize(3, 0.);
 	indexShift ++;
-	if (natom != 0){
-	  resdindex[natom+indexShift] = resdindex[natom-1] + indexShift + 1;
-	  atomindex[natom+indexShift] = atomindex[natom-1] + indexShift + 1;
-	}
-	else {
-	  resdindex[natom+indexShift] =  + indexShift + 1;
-	  atomindex[natom+indexShift] =  + indexShift + 1;
-	}
+	resdindex[natom+indexShift] = resdindex[natom-1] + indexShift + 1;
+	atomindex[natom+indexShift] = atomindex[natom-1] + indexShift + 1;
 	resdname[natom+indexShift] = "WA";
 	atomname[natom+indexShift] = "C";
 	posi[natom+indexShift].resize(3);
