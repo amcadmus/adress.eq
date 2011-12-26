913c7c9de6e1c92faf0bb0771ff9bdd4093a589c
wanghan <wanghan@a28d8dd4-3dfb-4ec0-b98a-e5cf011a4b41>
2011-12-26 14:42:02 +0000

    add template.run.with.tables

---
+++ template.run.with.tables/parameters.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+base_conf=tools/gen.conf/spc216.gro
+n_base_block="4 2 2"
+number_density=33.286      # 0.99577 g/cm^3
+
+table_dir=04_02_02
+
+ex_region_r=0.5
+
+gmx_nsteps=10000000
+gmx_nstenergy=200
+gmx_nstxtcout=200
---
+++ template.run.with.tables/run.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+source env.sh
+source parameters.sh
+
+mylog=`pwd`/run.log
+makelog=`pwd`/make.log
+rm -f $mylog
+
+# prepare conf.gro
+echo "# prepare conf.gro"
+## gen from base (default spc216.gro)
+echo "## gen from base (default spc216.gro)"
+genconf -f $base_conf -o conf.gro -nbox $n_base_block &>> $mylog
+## resize to right density step 1
+echo "## resize to right density step 1"
+boxx=`tail conf.gro -n 1 | awk '{print $1}'`
+boxy=`tail conf.gro -n 1 | awk '{print $2}'`
+boxz=`tail conf.gro -n 1 | awk '{print $3}'`
+natom=`head -n 2 conf.gro | tail -n 1`
+nmol=`echo "$natom / 3" | bc`
+now_density=`echo "$natom / 3 / ($boxx * $boxy * $boxz)" | bc -l`
+scale=`echo "($now_density / $number_density)" | bc -l`
+editconf -f conf.gro -o out.gro -scale $scale 1 1 &>> $mylog
+mv -f out.gro conf.gro
+boxx=`tail conf.gro -n 1 | awk '{print $1}'`
+boxy=`tail conf.gro -n 1 | awk '{print $2}'`
+boxz=`tail conf.gro -n 1 | awk '{print $3}'`
+## resize to right density step 2
+echo "## resize to right density step 2"
+newboxx=`printf "%.1f" $boxx`
+scalex=`echo "$newboxx / $boxx" | bc -l`
+scaleyz=`echo "sqrt(1./$scalex)" | bc -l`
+editconf -f conf.gro -o out.gro -scale $scalex $scaleyz $scaleyz &>> $mylog
+mv -f out.gro conf.gro
+boxx=`tail conf.gro -n 1 | awk '{print $1}'`
+boxy=`tail conf.gro -n 1 | awk '{print $2}'`
+boxz=`tail conf.gro -n 1 | awk '{print $3}'`
+## warm up run"
+echo "## warm up run"
+rm -fr warmup
+cp -a tools/atom.template ./warmup
+cd warmup
+mv ../conf.gro .
+sed "s/SOL.*/SOL $nmol/g" topol.top > tmp.top
+mv -f tmp.top topol.top
+grompp &>> $mylog
+mdrun -v &>> $mylog
+rm -fr ../conf.gro
+if test ! -f confout.gro; then
+    echo "no file confout.gro, exit!"
+    exit
+fi
+cp confout.gro ../conf.gro
+cd ..
+## add COM site"
+echo "## add COM site"
+make -C tools/gen.conf/ clean &> $makelog
+make -j4 -C tools/gen.conf/ &> $makelog
+tools/gen.conf/stupid.add.com -f conf.gro -o out.gro &>> $mylog
+mv -f out.gro conf.gro
+
+# prepare index file"
+echo "# prepare index file"
+echo "a COM" > command.tmp
+echo "name 3 CG" >> command.tmp
+echo "a HW1 HW2 OW" >> command.tmp
+echo "name 4 EX" >> command.tmp
+echo "q" >> command.tmp
+cat command.tmp  | make_ndx -f conf.gro &>> $mylog
+rm -fr command.tmp
+
+# prepare all tables
+echo "# prepare all tables"
+if test ! -d $table_dir; then
+    echo "no table dir $table_dir"
+    exit
+fi
+rm -f table*xvg
+cp -a $table_dir/table*xvg .
+
+# prepare grompp.mdp
+echo "# prepare grompp.mdp"
+hy_region_r=`grep hy_region_r $table_dir/parameters.sh | cut -d '=' -f 2`
+rm -fr grompp.mdp
+cp tools/tf.template/grompp.mdp .
+half_boxx=`echo "$boxx/2.0" | bc -l`
+half_boxy=`echo "$boxy/2.0" | bc -l`
+half_boxz=`echo "$boxz/2.0" | bc -l`
+if test `echo "($ex_region_r+$hy_region_r) >= $half_boxx" | octave | grep ans | cut -f 2 -d '='` -eq 1; then
+    echo "$ex_region_r+$hy_region_r" is bigger than half boxx, exit
+    exit
+fi
+sed -e "/^adress_ex_width/s/=.*/= $ex_region_r/g" grompp.mdp |\
+sed -e "/^adress_hy_width/s/=.*/= $hy_region_r/g" |\
+sed -e "/^nsteps/s/=.*/= $gmx_nsteps/g" |\
+sed -e "/^nstenergy/s/=.*/= $gmx_nstenergy/g" |\
+sed -e "/^nstxtcout/s/=.*/= $gmx_nstxtcout/g" |\
+sed -e "/^adress_reference_coords/s/=.*/= $half_boxx $half_boxy $half_boxz/g" > grompp.mdp.tmp
+mv -f grompp.mdp.tmp grompp.mdp
+
+# prepare topol.top
+echo "# prepare to tf"
+rm -fr topol.top
+cp tf.template/topol.top .
+sed "s/SOL.*/SOL $nmol/g" topol.top > tmp.top
+mv -f tmp.top topol.top
+
+
+
