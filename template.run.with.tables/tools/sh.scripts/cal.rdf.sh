9b061122a3f0e4c1d26a23ea50abdeeed9b17fa4
wanghan <wanghan@a28d8dd4-3dfb-4ec0-b98a-e5cf011a4b41>
2012-01-01 22:55:31 +0000

    make .9, .5 dir

--- template.run.with.tables/tools/sh.scripts/cal.rdf.sh
+++ template.run.with.tables/tools/sh.scripts/cal.rdf.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-intervals="3.75 4.25 4.80 5.35 5.90 6.45 7 7.5"
 intervals="3.75 4.25 5.16 6.08 7.00 7.50"
 make -C ./tools/analyze.rdf/ -j4 &> /dev/null
 
@@ -32,6 +31,48 @@ do
 done
 interval_end=$i
 
+rm -f rdf.inter.9
+mkdir rdf.inter.9
+mv rdf*out rdf.inter.9
+
 echo "./tools/analyze.rdf/cal.rdf -u 1.1 -c 1.15 -m $method -b 100 -e $time --x0 $interval_start --x1 $interval_end -o rdf.all.out"
 ./tools/analyze.rdf/cal.rdf -u 1.1 -c 1.15 -m $method -b 100 -e $time --x0 $interval_start --x1 $interval_end -o rdf.all.out
-echo 2 2| g_rdf -xvg none -bin .01 -b 100  -n index.ndx -dt 1 -rdf mol_com
+echo 2 2| g_rdf -xvg none -bin .01 -b 100  -n index.ndx -rdf mol_com
+
+
+intervals="3.75 4.25 4.80 5.35 5.90 6.45 7 7.5"
+c=0
+for i in $intervals;
+do
+    echo $i
+    if test $c -eq 0; then
+	c=$(($c+1))
+	lower=$i
+	interval_start=$i
+	continue
+    fi
+    upper=$i
+    print_lower=`echo "$lower * 100" | bc -l`
+    print_lower=`printf "%.0f" $print_lower`
+    print_upper=`echo "$upper * 100" | bc -l`
+    print_upper=`printf "%.0f" $print_upper`
+    nstep=`grep gmx_nsteps parameters.sh | cut -d '=' -f 2`
+    time=`echo "$nstep * 0.002" | bc -l`
+    if grep COM conf.gro &> /dev/null ; then
+	method=adress
+    else
+	method=atom
+    fi
+    echo "./tools/analyze.rdf/cal.rdf -u 1.1 -c 1.15 -m $method -b 100 -e $time --x0 $lower --x1 $upper -o rdf.$print_lower-$print_upper.out"
+    ./tools/analyze.rdf/cal.rdf -u 1.1 -c 1.15 -m $method -b 100 -e $time --x0 $lower --x1 $upper -o rdf.$print_lower-$print_upper.out
+    lower=$upper
+done
+interval_end=$i
+
+rm -f rdf.inter.5
+mkdir rdf.inter.5
+mv rdf*out rdf.inter.5
+
+echo "./tools/analyze.rdf/cal.rdf -u 1.1 -c 1.15 -m $method -b 100 -e $time --x0 $interval_start --x1 $interval_end -o rdf.all.out"
+./tools/analyze.rdf/cal.rdf -u 1.1 -c 1.15 -m $method -b 100 -e $time --x0 $interval_start --x1 $interval_end -o rdf.all.out
+
