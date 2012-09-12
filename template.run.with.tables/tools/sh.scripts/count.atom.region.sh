ac610750ab9db6c96ec5aeb6800677360fc5e920
wanghan <wanghan@a28d8dd4-3dfb-4ec0-b98a-e5cf011a4b41>
2012-09-12 14:20:16 +0000

    add count.atom.region.sh

---
+++ template.run.with.tables/tools/sh.scripts/count.atom.region.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "use: "
+    echo "count.interval.sh method"
+    exit
+fi
+method=$1
+
+source parameters.sh
+boxx=`tail -n 1 confout.gro | awk '{print $1}'`
+boxxh=`echo "$boxx * 0.5" | bc -l`
+x0=`echo "$boxxh - $ex_region_r" | bc -l`
+x1=`echo "$boxxh +  $ex_region_r" | bc -l`
+yz1=`tail -n 1 confout.gro | awk '{print $2}'`
+
+command="./tools/analyze.number/count.interval -b 500 --x0 $x0 --x1 $x1 --yz1 $yz1 -m $method "
+echo command is $command
+$command
+
+
